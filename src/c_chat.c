cd0dbcb7e9bca6c7377d2f3a06b98ec9a60826ae
khaledzh12 <khaledzh12313@gmail.com>
2026-08-10 14:39:13 +0300

    write create message and send message

--- src/c_chat.c
+++ src/c_chat.c
@@ -1,1 +1,33 @@
 #include "../include/c_chat.h"
+
+
+payload_t *create_message(const user_t *user, const char *message){
+ if(!message || !user) return NULL;
+
+    payload_t * payload = (payload_t)malloc(sizeof(payload_t));
+    if(!message) return NULL;
+
+    message->user_metadata = *user;
+    message->type = MESSAGE;
+    message->message_len = size;
+    return message ;
+
+
+}
+
+bool send_message(int socket_fd, const payload_t *message){
+    if(!message || !socket_fd) {
+
+        printf("Error: Invalid message or socket_fd\n");
+    
+        return false;
+        }
+    
+size_t sent = send (socket_fd, message, sizof(payload_t),0);
+
+
+    if(sent != sizeof(payload_t)){
+        return false;
+    }
+    return true;
+}
