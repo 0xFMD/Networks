47fd980782fa787671b068b51e568a1b2c55662c
0xFMD <30713087+0xFMD@users.noreply.github.com>
2026-08-10 13:39:07 +0300

    feat(c_chat/user): add create, delete

--- src/c_chat.c
+++ src/c_chat.c
@@ -1,1 +1,21 @@
 #include "../include/c_chat.h"
+
+user_t *create_user(const char *username, const char *ip) {
+  if (!username || !ip)
+    return NULL;
+
+  user_t *user = malloc(sizeof(user_t));
+
+  if (!user)
+    return NULL;
+
+  strcpy(user->username, username);
+  strcpy(user->ip, ip);
+
+  return user;
+}
+
+void delete_user(user_t *user) {
+  if (user)
+    free(user);
+}
