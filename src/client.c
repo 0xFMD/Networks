9db934c2a84500f00d18946ad52bef0758bfc270
khaledzh12 <khaledzh12313@gmail.com>
2026-08-10 17:15:03 +0300

    write connect to server

--- src/client.c
+++ src/client.c
@@ -1,3 +1,37 @@
 #include "../include/client.h"
 
-int main(void) { return 0; }
+
+int connect_to_server(const char *host, const uint16_t port){
+    
+    struct sockaddr_in server_addr;
+
+int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+if (socket_fd < 0) {
+        perror("socket");
+        exit(1);
+    }
+
+     memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(PORT);
+
+    if (inet_pton(AF_INET, host, &server_addr.sin_addr) <= 0) {
+        perror("inet_pton");
+        exit(1);
+    }
+
+     if (connect(client_fd, (struct sockaddr *)&s, sizeof(server_addr)) < 0) {
+        perror("connect");
+        close(socket_fd);
+    }
+
+    return socket_fd;
+}
+int main(void) {
+   
+    
+
+
+    return 0; 
+}
